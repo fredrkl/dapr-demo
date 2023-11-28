f5fabf78c478c655d67eeba40f76d7e8766ef6d9
Fredrik Klingenberg <fredrkl@gmail.com>
2023-11-28 20:44:27 +0100

    feat: caller making a single http request
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

---
+++ service-invocation/caller/Program.cs
@@ -0,0 +1,17 @@
+﻿//var client = new HttpClient();
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+var client = new HttpClient();
+var response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
+
+if (response.IsSuccessStatusCode)
+{
+  var content = await response.Content.ReadAsStringAsync();
+  Console.WriteLine(content);
+}
+else
+{
+  Console.WriteLine($"Request failed with status code {response.StatusCode}");
+}
