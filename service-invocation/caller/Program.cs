898e8626d0867cffd46015331372ea1397798464
Fredrik Klingenberg <fredrkl@gmail.com>
2023-11-29 06:52:31 +0100

    feat: demo endpoint calls
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- service-invocation/caller/Program.cs
+++ service-invocation/caller/Program.cs
@@ -1,12 +1,17 @@
 ﻿var client = new HttpClient();
-var response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
 
-if (response.IsSuccessStatusCode)
+for (int i = 1; i <= 100; i++)
 {
-  var content = await response.Content.ReadAsStringAsync();
-  Console.WriteLine(content);
-}
-else
-{
-  Console.WriteLine($"Request failed with status code {response.StatusCode}");
+  var response = await client.GetAsync($"https://jsonplaceholder.typicode.com/todos/{i}");
+
+  if (response.IsSuccessStatusCode)
+  {
+    var content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(content);
+  }
+  else
+  {
+    Console.WriteLine($"Request failed with status code {response.StatusCode}");
+  }
+  Thread.Sleep(2000);
 }
